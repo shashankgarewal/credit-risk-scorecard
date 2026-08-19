131cd887a7e3a29918cda97753e9fecb4b3c7808
shashankgarewal <shashankgarewal4+github@gmail.com>
2026-08-20 03:26:33 +0530

    feat: replace logging.config with stream and file handlers, and file rotating handler

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,20 +1,38 @@
 import os
+import sys
 import logging
+from logging.handlers import TimedRotatingFileHandler
 from dotenv import load_dotenv
-from datetime import datetime
+from src.utils.config import LOG_DIR
 
 load_dotenv()
-is_dev_env = bool(os.getenv('DEV_ENV'))
+is_dev_env = (os.getenv("DEV_ENV") or "").lower() in ("true", "1")
+
+logger = logging.getLogger(__name__)
 
 if is_dev_env:
-    log_dir = os.path.join(os.getcwd(), 'logs')
-    os.makedirs(log_dir, exist_ok=True)
-    log_path = os.path.join(log_dir, f"{datetime.now().date()}.log")
+    logger.setLevel(logging.DEBUG)
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+    # rotate logs every day and keep 14 days of logs
+    file_handler = TimedRotatingFileHandler(
+        LOG_DIR / "app.log", when="midnight", backupCount=14
+    )
+    file_handler.setFormatter(logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(filename)s | %(funcName)s:%(lineno)d | %(message)s,",
+        datefmt="%H:%M:%S"
+    ))
+    logger.addHandler(file_handler)
+
+    # also stdout to console in dev
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(logging.Formatter("%(levelname)s | %(message)s"))
+    logger.addHandler(console_handler)
+
 else:
-    log_path = None
-    
-logging.basicConfig(
-    filename=log_path,
-    level=logging.DEBUG,
-    format='%(asctime)s | %(levelname)s | %(filename)s | %(funcName)s:%(lineno)d | %(message)s',
-)
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    ))
+    logger.addHandler(console_handler)
